54ed5592fd603f1f38b6ae0bab44bc90ee4c6704
Enhaasa <enhaasa@pm.me>
2023-08-27 00:11:39 +0200

    Split up venues and communities in partner section.

--- src/sections/Partners.tsx
+++ src/sections/Partners.tsx
@@ -21,23 +21,37 @@ function Partners({ handleModal }: Props) {
     const venuePartners = partners ? partners.filter(partner => partner.level === "venue") : [];
     const communityPartners = partners ? partners.filter(partner => partner.level === "community") : [];
 
-    getExternal.db_cache("partners").then(data => {
-        setPartners(data);
-    });
+    useEffect(() => {
+        getExternal.db_cache("partners").then(data => {
+            setPartners(data);
+        });
+    }, []);
 
     function trimFull(string:string):string {
         return string.replace(/\s/g, '');
     }
 
-
     return (
-        <div className="partners">
-            {partners && partners.map(partner => (
-                <a className="partner" href={partner.website} target="_blank" rel="noreferrer" key={partner.id}>
-                    <img src={`${sources.cdn}/partners/${trimFull(partner.name)}.webp`} />
-                    <div className="name">{partner.name}</div>
-                </a>
-            ))}
+        <div className='partners'>
+            <div className='title'>Venues</div>
+            <div className="items">
+                {venuePartners && venuePartners.map(partner => (
+                    <a className="item" href={partner.website} target="_blank" rel="noreferrer">
+                        <img src={`${sources.cdn}/partners/${trimFull(partner.name)}.webp`} alt={partner.name}/>
+                        <div className="name">{partner.name}</div>
+                    </a>
+                ))}
+            </div>
+
+            <div className='title'>Communities</div>
+            <div className="items">
+                {communityPartners && communityPartners.map(partner => (
+                    <a className="item" href={partner.website} target="_blank" rel="noreferrer">
+                        <img src={`${sources.cdn}/partners/${trimFull(partner.name)}.webp`} alt={partner.name}/>
+                        <div className="name">{partner.name}</div>
+                    </a>
+                ))}
+            </div>
         </div>
     );
 }
